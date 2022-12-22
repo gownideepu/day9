2a481b00ece1c480bb061a8df0b40833d324d6d5
unknown <gownideepu23@gmail.com>
2022-12-22 17:21:08 +0530

    updated

--- uc1.sh
+++ uc1.sh
@@ -1,28 +1,38 @@
-is_prent=$((RANDOM%3));
 sal_hour=20;
 working_hour=0;
-case $is_prent in 
-		0)
-				echo "employee is apsent ";
-				working_hour=0;
+month_sal=0;
 
-		;;
+for ((day=1; day<=20; day++))
+do
+
+	is_present=$((RANDOM%3));
+	case $is_present in 
+			0)
+					echo "employee is apsent ";
+					working_hour=0;
 	
-		1)
-				echo "employee is present ";
-				working_hour=8;
-				 
-		;;
-		2)
-				echo "employee as part time";
+			;;
+		
+			1)
+					echo "employee is present ";
+					working_hour=8;
+				 	
+			;;
+			2)
+					echo "employee as part time";
 
-				working_hour=4;
-		;;
+					working_hour=4;
+			;;
+	
+	esac
 
-esac
+	ttl_salary=$(($sal_hour * $working_hour));
+	
+	echo  " total salary of employee $ttl_salary ";
+	
+	month_sal=$(($month_sal + $ttl_salary));
 
-ttl_salary=$(($sal_hour * $working_hour));
+done
 
 
-
-echo  " total salary of employee $ttl_salary ";
+echo  " total salary of employee in month $month_sal ";
